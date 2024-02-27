0b44dd6dcbc84ca522008bc7a7df6699ad92b5ff
Pacotyse <e.tison85@gmail.com>
2024-02-27 13:13:35 +0100

    Adding documentation to the book model

--- src/main/java/com/Pacotyse/LibriSync/model/Book.java
+++ src/main/java/com/Pacotyse/LibriSync/model/Book.java
@@ -5,6 +5,9 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 
+/**
+ * Represents a book in the library management system.
+ */
 @Entity
 public class Book {
 
@@ -12,34 +15,68 @@ public class Book {
     @GeneratedValue
     private Long id;
 
+    /**
+     * The title of the book.
+     */
     private @Column(name = "title") String title;
+
+    /**
+     * The author of the book.
+     */
     private @Column(name = "author") String author;
 
-    public Book() {};
+    /**
+     * Default constructor.
+     */
+    public Book() {}
 
+    /**
+     * Constructs a book with the given title and author.
+     * @param title The title of the book.
+     * @param author The author of the book.
+     */
     public Book(String title, String author) {
         this.title = title;
         this.author = author;
     }
 
+    /**
+     * Gets the id of the book.
+     * @return The id of the book.
+     */
     public Long getId() {
         return id;
     }
 
+    /**
+     * Gets the title of the book.
+     * @return The title of the book.
+     */
     public String getTitle() {
         return title;
     }
 
+    /**
+     * Sets the title of the book.
+     * @param title The new title of the book.
+     */
     public void setTitle(String title) {
         this.title = title;
     }
 
+    /**
+     * Gets the author of the book.
+     * @return The author of the book.
+     */
     public String getAuthor() {
         return author;
     }
 
+    /**
+     * Sets the author of the book.
+     * @param author The new author of the book.
+     */
     public void setAuthor(String author) {
         this.author = author;
     }
-
 }
