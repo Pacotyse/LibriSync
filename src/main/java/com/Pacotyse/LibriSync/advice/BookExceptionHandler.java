66a12f22b1ffde3aeb3bbf7c558bb12b849ca6f1
Pacotyse <e.tison85@gmail.com>
2024-02-29 11:55:07 +0100

    Adding documentation to the Book exception and handler

--- src/main/java/com/Pacotyse/LibriSync/advice/BookExceptionHandler.java
+++ src/main/java/com/Pacotyse/LibriSync/advice/BookExceptionHandler.java
@@ -1,17 +1,27 @@
-package com.Pacotyse.LibriSync.exception;
+package com.Pacotyse.LibriSync.advice;
 
+import com.Pacotyse.LibriSync.exception.BookNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
+/**
+ * Advice class to handle exceptions related to Book entities.
+ */
 @ControllerAdvice
-public class BookNotFoundAdvice {
+public class BookExceptionHandler {
+
+    /**
+     * Handles BookNotFoundException and returns a 404 NOT_FOUND response.
+     * @param ex The BookNotFoundException instance.
+     * @return A string representing the error message.
+     */
     @ResponseBody
     @ExceptionHandler(BookNotFoundException.class)
     @ResponseStatus(HttpStatus.NOT_FOUND)
-    String employeeNotFoundHandler(BookNotFoundException ex) {
+    String bookNotFoundHandler(BookNotFoundException ex) {
         return ex.getMessage();
     }
 }
