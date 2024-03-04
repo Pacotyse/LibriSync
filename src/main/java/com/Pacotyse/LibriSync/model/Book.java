43fc0b7e8bbff141a22912c222f74e567ca10c82
Pacotyse <e.tison85@gmail.com>
2024-03-04 10:30:36 +0100

    New dependency Lombok handling all the generic methods (get, set, hashCode, .etc) and refactoring models Member and Book

--- src/main/java/com/Pacotyse/LibriSync/model/Book.java
+++ src/main/java/com/Pacotyse/LibriSync/model/Book.java
@@ -3,12 +3,16 @@ package com.Pacotyse.LibriSync.model;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotNull;
+import lombok.Data;
 
 /**
  * Represents a book in the library management system.
  */
 @Entity
+@Data
+@Table(name = "book")
 public class Book {
 
     @Id
@@ -38,103 +42,4 @@ public class Book {
      */
     private @NotNull
     @Column(name = "stock") int stock = 1;
-
-    /**
-     * Default constructor.
-     */
-    public Book() {}
-
-    /**
-     * Constructs a book with the specified details.
-     * @param isbn The ISBN of the book.
-     * @param title The title of the book.
-     * @param author The author of the book.
-     * @param editor The editor of the book.
-     */
-    public Book(Long isbn, String title, String author, String editor) {
-        this.isbn = isbn;
-        this.title = title;
-        this.author = author;
-        this.editor = editor;
-    }
-
-    /**
-     * Gets the ISBN of the book.
-     * @return The ISBN of the book.
-     */
-    public Long getIsbn() {
-        return isbn;
-    }
-
-    /**
-     * Sets the ISBN of the book.
-     * @param isbn The new ISBN of the book.
-     */
-    public void setIsbn(Long isbn) {
-        this.isbn = isbn;
-    }
-
-    /**
-     * Gets the title of the book.
-     * @return The title of the book.
-     */
-    public String getTitle() {
-        return title;
-    }
-
-    /**
-     * Sets the title of the book.
-     * @param title The new title of the book.
-     */
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    /**
-     * Gets the author of the book.
-     * @return The author of the book.
-     */
-    public String getAuthor() {
-        return author;
-    }
-
-    /**
-     * Sets the author of the book.
-     * @param author The new author of the book.
-     */
-    public void setAuthor(String author) {
-        this.author = author;
-    }
-
-    /**
-     * Gets the editor of the book.
-     * @return The editor of the book.
-     */
-    public String getEditor() {
-        return editor;
-    }
-
-    /**
-     * Sets the editor of the book.
-     * @param editor The new editor of the book.
-     */
-    public void setEditor(String editor) {
-        this.editor = editor;
-    }
-
-    /**
-     * Gets the stock quantity of the book.
-     * @return The stock quantity of the book.
-     */
-    public int getStock() {
-        return stock;
-    }
-
-    /**
-     * Sets the stock quantity of the book.
-     * @param stock The new stock quantity of the book.
-     */
-    public void setStock(int stock) {
-        this.stock = stock;
-    }
 }
--- src/main/java/com/Pacotyse/LibriSync/model/Member.java
+++ src/main/java/com/Pacotyse/LibriSync/model/Member.java
@@ -3,25 +3,28 @@ package com.Pacotyse.LibriSync.model;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotNull;
+import lombok.Data;
 
 /**
  * Represents a member in the library system.
  */
 @Entity
+@Data
+@Table(name = "member")
 public class Member {
 
     /**
      * The unique identifier for the member.
      */
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     /**
      * The first name of the member.
      */
-    @NotNull
     @Column(name = "first_name")
+    @NotNull
     private String first_name;
 
     /**
@@ -38,85 +41,4 @@ public class Member {
     @NotNull
     @Column(name = "email", unique = true)
     private String email;
-
-    /**
-     * Default constructor.
-     */
-    public Member() {}
-
-    /**
-     * Constructs a member with the given parameters.
-     * @param first_name The first name of the member.
-     * @param last_name The last name of the member.
-     * @param email The email address of the member.
-     */
-    public Member(String first_name, String last_name, String email) {
-        this.first_name = first_name;
-        this.last_name = last_name;
-        this.email = email;
-    }
-
-    /**
-     * Gets the id of the member.
-     * @return The id of the member.
-     */
-    public Long getId() {
-        return id;
-    }
-
-    /**
-     * Sets the id of the member.
-     * @param id The id of the member.
-     */
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    /**
-     * Gets the first name of the member.
-     * @return The first name of the member.
-     */
-    public String getFirst_name() {
-        return first_name;
-    }
-
-    /**
-     * Sets the first name of the member.
-     * @param first_name The first name of the member.
-     */
-    public void setFirst_name(String first_name) {
-        this.first_name = first_name;
-    }
-
-    /**
-     * Gets the last name of the member.
-     * @return The last name of the member.
-     */
-    public String getLast_name() {
-        return last_name;
-    }
-
-    /**
-     * Sets the last name of the member.
-     * @param last_name The last name of the member.
-     */
-    public void setLast_name(String last_name) {
-        this.last_name = last_name;
-    }
-
-    /**
-     * Gets the email address of the member.
-     * @return The email address of the member.
-     */
-    public String getEmail() {
-        return email;
-    }
-
-    /**
-     * Sets the email address of the member.
-     * @param email The email address of the member.
-     */
-    public void setEmail(String email) {
-        this.email = email;
-    }
 }
